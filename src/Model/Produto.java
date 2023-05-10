bd0da63aa412fd7a29550993aba56ea15c32bca9
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2023-05-10 18:47:03 -0300

    Teste

---
+++ src/Model/Produto.java
@@ -0,0 +1,44 @@
+package Model;
+
+public class Produto {
+    private int id;
+    private String nome;
+    private float valor;
+
+    public void editarProduto(int id, String nome, float valor){
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+
+    }
+
+    public Produto(int id, String nome, float valor) {
+        this.id = id;
+        this.nome = nome;
+        this.valor = valor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public float getValor() {
+        return valor;
+    }
+
+    public void setValor(float valor) {
+        this.valor = valor;
+    }
+}
