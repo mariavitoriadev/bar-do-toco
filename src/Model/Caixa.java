bd5ac2eefae830d914b0af1961272aa092bbf542
mariavitoriadev <mariavitoriani@gmail.com>
2023-05-10 20:44:22 -0300

    cria as funções da classe caixa

--- src/Model/Caixa.java
+++ src/Model/Caixa.java
@@ -5,11 +5,25 @@ public class Caixa {
     private Status status;
     private static Caixa INSTANCE;
 
-    private Caixa(){}
+    private Caixa(){
+        this.status = Status.FECHADO;
+    }
 
     public synchronized static Caixa getInstance(){
         if(INSTANCE == null) INSTANCE = new Caixa();
         return INSTANCE;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void abrirCaixa() {
+        status = Status.ABERTO;
+    }
+
+    public void fecharCaixa() {
+        status = Status.FECHADO;
     }
 
 }
