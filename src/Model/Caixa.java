0b35d61ba207fc8a27e689cb31f26b05ab5c9a42
mariavitoriadev <mariavitoriani@gmail.com>
2023-05-10 20:18:12 -0300

    create Caixa singleton

--- src/Model/Caixa.java
+++ src/Model/Caixa.java
@@ -2,6 +2,14 @@ package Model;
 
 public class Caixa {
 
-    private Status status; 
+    private Status status;
+    private static Caixa INSTANCE;
+
+    private Caixa(){}
+
+    public synchronized static Caixa getInstance(){
+        if(INSTANCE == null) INSTANCE = new Caixa();
+        return INSTANCE;
+    }
 
 }
