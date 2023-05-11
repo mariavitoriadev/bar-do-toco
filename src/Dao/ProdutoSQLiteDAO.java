45d291228a6c7015a928f384f2991c22414e6c4e
mariavitoriadev <mariavitoriani@gmail.com>
2023-05-10 22:14:25 -0300

    cria DAO do produto

---
+++ src/Dao/ProdutoSQLiteDAO.java
@@ -0,0 +1,67 @@
+package Dao;
+
+import Model.Produto;
+import java.sql.*;
+
+public class ProdutoSQLiteDAO implements ProdutoDAO{
+
+    @Override
+    public void criarTabela() {
+
+        String sql = "CREATE TABLE IF NOT EXISTS produto (" +
+                "id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "nome TEXT NOT NULL, " +
+                "valor REAL NOT NULL )";
+
+        try{
+            PreparedStatement stmt = ConnectionFactory.createStatement(sql);
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+    }
+    @Override
+    public void salvar(Produto produto) {
+        String sql = "INSERT INTO produto values (?,?,?)";
+
+        try(
+            PreparedStatement stmt = ConnectionFactory.createStatement(sql)){
+            stmt.setInt(1,produto.getId());
+            stmt.setString(2,produto.getNome());
+            stmt.setDouble(3,produto.getValor());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    };
+
+    @Override
+    public void atualizar(Produto produto) {
+        String sql = "UPDATE produto SET name=?, valor=? WHERE id=?";
+
+        try(
+            PreparedStatement stmt = ConnectionFactory.createStatement(sql)){
+            stmt.setString(1,produto.getNome());
+            stmt.setDouble(2,produto.getValor());
+            stmt.setInt(3,produto.getId());
+            stmt.executeUpdate();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    };
+
+    @Override
+    public void apagar(Produto produto) {
+        String sql = "DELETE FROM produto WHERE id=?";
+
+        try(
+            PreparedStatement stmt = ConnectionFactory.createStatement(sql)){
+            stmt.setInt(1,produto.getId());
+            stmt.executeUpdate();
+        }catch (SQLException e) {
+            e.printStackTrace();
+        }
+    };
+
+}
