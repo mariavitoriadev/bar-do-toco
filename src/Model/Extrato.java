ee65d17dffdd6ed0681bf1087c9f702603afe0ee
tonyviniciuscavalcante <tonycavalcante123@yahoo.com.br>
2023-05-10 21:52:33 -0300

    classes restantes

--- src/Model/Extrato.java
+++ src/Model/Extrato.java
@@ -6,22 +6,3 @@ public class Extrato implements Comparator<int> {
     private int valorTotal;
     Map<int, String=""> valorDasContas;
 }
-
-
-
-
-
-
-public class ComparatorInts implements Comparator<integer> {
-
-    Map<integer, string=""> base;
-
-    public ComparatorInts(Map<integer, string=""> base) {
-        this.base = base;
-    }
-
-    @Override
-    public int compare(Integer o1, Integer o2) {
-        return base.get(o1).compareTo(base.get(o2));
-    }
-}
