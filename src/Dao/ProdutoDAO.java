248786cb9c0f66ebc95eac949545e61952c67aac
Willian Cristian <44729804+wcristiansilva@users.noreply.github.com>
2023-05-11 22:17:57 -0300

    #

--- src/Dao/ProdutoDAO.java
+++ src/Dao/ProdutoDAO.java
@@ -6,7 +6,7 @@ import java.util.List;
 public interface ProdutoDAO {
 
     void criarTabela();
-    void  salvar(Produto produto);
+    void salvar(Produto produto);
     void atualizar (Produto produto);
     void apagar (Produto produto);
 
